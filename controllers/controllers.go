f8add8820a588bda0595eb61d294a45fefe87972
Cali0p3 <robertta.longoni@gmail.com>
2023-10-02 10:48:16 -0300

    Modularizando o projeto

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,17 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/cali0p3/api-go-gin/models"
+	"github.com/gin-gonic/gin"
+)
 
 func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, models.Alunos)
+}
+
+func Saudacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Caliope Longoni",
+		"API diz:": "Olá " + nome + ", tudo tranquilo?",
 	})
 }
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/cali0p3/api-go-gin/routes"
+import (
+	"github.com/cali0p3/api-go-gin/models"
+	"github.com/cali0p3/api-go-gin/routes"
+)
 
 func main() {
+	models.Alunos = []models.Aluno {
+		{Nome: "Calíope Longoni", CPF: "12345678901", RG: "123456789"},
+		{Nome: "Anna Ball", CPF: "21345678901", RG: "987654321"},
+	}
 	routes.HandleRequests()
 }
