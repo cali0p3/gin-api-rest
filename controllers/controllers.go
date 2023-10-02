780694c45f27768faa78a5318f1053a3e9326ddc
Cali0p3 <robertta.longoni@gmail.com>
2023-10-02 11:23:25 -0300

    Conectando database e exibindo alunos

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/cali0p3/api-go-gin/database"
 	"github.com/cali0p3/api-go-gin/models"
 	"github.com/gin-gonic/gin"
 )
 
 func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, models.Alunos)
+	var alunos []models.Aluno
+	database.DB.Find(&alunos)
+	c.JSON(200, alunos)
 }
 
 func Saudacao(c *gin.Context) {
@@ -14,4 +19,15 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz:": "Olá " + nome + ", tudo tranquilo?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err!= nil{
+		c.JSON(http.StatusBadGateway, gin.H{
+			"error": err.Error()})
+			return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"github.com/cali0p3/api-go-gin/models"
+	"github.com/cali0p3/api-go-gin/database"
+	
 	"github.com/cali0p3/api-go-gin/routes"
 )
 
 func main() {
-	models.Alunos = []models.Aluno {
-		{Nome: "Calíope Longoni", CPF: "12345678901", RG: "123456789"},
-		{Nome: "Anna Ball", CPF: "21345678901", RG: "987654321"},
-	}
+	database.ConectaComBancoDeDados()
+	
 	routes.HandleRequests()
 }
